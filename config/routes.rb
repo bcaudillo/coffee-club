68102979ff18012f75ea221fc97408dc3e21c03e
Brandon Caudillo <52050659+bcaudillo@users.noreply.github.com>
2025-07-29 11:43:50 -0600

    refactor routes to be nested

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
-Rails.application.routes.draw do
-resources :coffees, only: [:index, :create, :destroy, :update, :show]
-resources :reviews, only: [:create, :index, :destroy, :update]
-
-  post "/login", to: "sessions#create"
-  delete "/logout", to: "sessions#destroy"
-
-  post "/signup", to: "users#create"
-  get "/users", to: "users#index"
-  get "/me", to: "users#show"
-  get "/users/blends", to: "users#blends"
-
+namespace :api do
+  namespace :v1 do
+    resources :coffees, only: [:index, :create, :destroy, :update, :show] do
+      resources :reviews, only: [:create, :index, :destroy, :update]
+    end
+    resources :reviews, only: [:create, :index, :destroy, :update] # If you still need top-level reviews
+    post "/login", to: "sessions#create"
+    delete "/logout", to: "sessions#destroy"
+    post "/signup", to: "users#create"
+    get "/me", to: "users#show"
+    get "/users/blends", to: "users#blends" # Or /api/v1/me/blends
+  end
 end
